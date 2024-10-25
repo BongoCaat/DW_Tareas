9284bbb50b9e1a94da26ff6225f472b50bcdd1b7
BongoCaat <valentinopietropaolo70@gmail.com>
2024-10-25 18:56:53 -0300

    feat: Agrega funcionalidad para actualizar usuarios
    Se ha añadido la capacidad de actualizar la información de los usuarios existentes. Ahora los usuarios pueden modificar el nombre y el correo electrónico de un usuario de la lista. Se ha mejorado la interfaz para facilitar la selección y edición de la información del usuario.

--- src/APIWeb.jsx
+++ src/APIWeb.jsx
@@ -4,6 +4,9 @@ function APIWeb() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [users, setUsers] = useState([]);
+    const [userSelect, setUserSelect] = useState(null);
+
+    const apiUrl = "https://6718464eb910c6a6e02b84fb.mockapi.io/usuarios"
 
     const addUser = async () => {
         if (!name.trim() || !email.trim()) {
@@ -13,7 +16,7 @@ function APIWeb() {
         const newUser = { name, email };
 
         try {
-            const response = await fetch('https://6718464eb910c6a6e02b84fb.mockapi.io/usuarios', {
+            const response = await fetch((apiUrl), {
                 method: 'POST',
                 body: JSON.stringify(newUser),
                 headers: {
@@ -36,7 +39,7 @@ function APIWeb() {
 
     const getUsers = async () => {
         try {
-            const response = await fetch('https://6718464eb910c6a6e02b84fb.mockapi.io/usuarios');
+            const response = await fetch(apiUrl);
             if (response.ok) {
                 const data = await response.json();
                 setUsers(data);
@@ -46,7 +49,7 @@ function APIWeb() {
         } catch (error) {
             console.log("Hubo un error en la solicitud para conseguir la lista:", error);
         }
-    };
+    }
 
     const deleteUser = async (id) => {
         const secretKey = prompt("Ingrese la clave secreta: ");
@@ -56,7 +59,7 @@ function APIWeb() {
         }
 
         try {
-            const response = await fetch(`https://6718464eb910c6a6e02b84fb.mockapi.io/usuarios/${id}`, {
+            const response = await fetch(`${apiUrl}/${id}`, {
                 method: 'DELETE',
             });
             if (response.ok) {
@@ -66,6 +69,44 @@ function APIWeb() {
             }
         } catch (error) {
             console.log("Hubo un error en la solicitud para borrar el usuario:", error);
+        }
+    }
+
+    const updateUser = async (id) => {
+        if (!userSelect) {
+            console.log("No hay ningun usuario seleccionado para actualizar.");
+            return;
+        }
+
+        if (!name.trim() || !email.trim()) {
+            alert('Por favor, complete todos los campos.');
+            return;
+        }
+
+        const updatedUser = { name, email };
+
+        try {
+            const response = await fetch(`${apiUrl}/${userSelect.id}`, {
+                method: 'PUT',
+                body: JSON.stringify(updatedUser),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setUsers(users.map((user) => (user.id === userSelect.id === id ? data : user)));
+                console.log(data);
+                setUserSelect(null);
+                setName('');
+                setEmail('');
+            } else {
+                console.log("Hubo un error al intentar actualizar el usuario.");
+            }
+        } catch (error) {
+            console.log("Hubo un error en la solicitud para actualizar el usuario:", error);
+        } finally {
+            getUsers();
         }
     }
 
@@ -79,19 +120,29 @@ function APIWeb() {
                 <h1 className="textoHead1 mb-2">Formulario</h1>
 
                 <label  className="labText" htmlFor="name">Nombre: </label>
-                <input id="name" className="form-control" type="text" value={name} maxLength={50} placeholder="Nombre" onChange={(e) => setName(e.target.value)} />
+                <input id="name" className="form-control" type="text" value={name} maxLength={50} placeholder={userSelect ? userSelect.name : "Nombre"} onChange={(e) => setName(e.target.value)} />
 
                 <label  className="labText" htmlFor="email">Email: </label>
-                <input id="email" className="form-control" type="email" value={email} maxLength={50} placeholder="correo@gmail.com" onChange={(e) => setEmail(e.target.value)} />
+                <input id="email" className="form-control" type="email" value={email} maxLength={50} placeholder={userSelect ? userSelect.email : "correo@gmail.com"} onChange={(e) => setEmail(e.target.value)} />
 
-                <button className="btn btn-outline-primary" onClick={addUser}>Agregar Usuario</button>
+                {userSelect ? (
+                    <div id="selectButtons">
+                        <button className="btn btn-outline-info" onClick={updateUser}>Actualizar Usuario</button>
+                        <button className="btn btn-outline-danger" onClick={() => setUserSelect(null)}>Cancelar</button>
+                    </div>
+                ) : (
+                    <button className="btn btn-outline-primary" onClick={addUser}>Agregar Usuario</button>
+                )}
             </div>
 
             <div id="ListUsers">
                 <h1 className="textoHead1 mt-4">Lista de Usuarios</h1>
                 <h3><a className="name link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="https://6718464eb910c6a6e02b84fb.mockapi.io/usuarios" target="_blank">Base de datos</a></h3>
                 {users.length === 0 ? (
-                    <p style={{fontSize: '24px'}}>No hay usuarios en la lista.</p>
+                    <div style={{textAlign: 'center'}}>
+                        <p style={{fontSize: '36px'}}>No hay usuarios en la lista.</p>
+                        <i className="bx bx-loader bx-spin bx-rotate-180" style={{fontSize: '10rem'}} />
+                    </div>
                 ) : (
                     <div id="listaCont">
                         <ul className="lista-tareas list-group list-group-numbered">
@@ -100,6 +151,7 @@ function APIWeb() {
                                 <span className="textSpan">Usuario : {user.name}</span>
                                 <span className="textSpan">Email : {user.email}</span>
                                 <button className="btn btn-outline-danger" onClick={() => deleteUser(user.id)}>Eliminar</button>
+                                <button className="btn btn-outline-info" onClick={() => setUserSelect(user)}>Actualizar</button>
                             </li>
                         ))}
                         </ul>
